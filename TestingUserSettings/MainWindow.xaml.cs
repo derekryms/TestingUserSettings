2a70fec64aa690654b5c92b2c953f71de67b7714
derek <derek@DEREKSPC>
2022-03-08 00:00:55 -0500

    Add project files.

---
+++ TestingUserSettings/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+
+namespace TestingUserSettings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (Properties.Settings.Default.UpdatedNeeded)
+            {
+                Properties.Settings.Default.Upgrade();
+                Properties.Settings.Default.UpdatedNeeded = false;
+                Properties.Settings.Default.Save();
+            }
+
+            firstNameTBox.Text = Properties.Settings.Default.FirstName;
+            lastNameTBox.Text = Properties.Settings.Default.LastName;
+            updateNeededTBlock2.Text = Properties.Settings.Default.UpdatedNeeded.ToString();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.FirstName = firstNameTBox.Text;
+            Properties.Settings.Default.LastName = lastNameTBox.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
